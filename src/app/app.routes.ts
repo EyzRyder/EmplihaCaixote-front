0720cfa2c53b4487c49a5252d2d5d08aa1b7ee59
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-20 01:07:40 -0300

    chore: make navigatio btn in home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { RoomsComponent } from './pages/rooms/rooms.component';
 import { LobbyComponent } from './pages/lobby/lobby.component';
 import { GameComponent } from './pages/game/game.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'rooms', component: RoomsComponent },
   { path: 'room/:id', component: LobbyComponent },
   { path: 'game', component: GameComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
 
   // rota padrão
-  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  //{ path: '', redirectTo: 'rooms', pathMatch: 'full' },
   { path: '**', redirectTo: 'rooms' },
 ];
