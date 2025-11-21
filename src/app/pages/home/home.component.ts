1942cf05f19c6eba716e3ac369de355d3ca8ab1a
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-20 23:44:29 -0300

    feat: show difrent btn if login or not

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(public authService: UserService) {}
+}
