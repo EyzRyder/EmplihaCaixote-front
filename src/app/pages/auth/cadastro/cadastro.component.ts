4c60376cf8f6803bc7d8f953cf44840c13f43993
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-21 00:05:07 -0300

    feat: implament cadastro page

--- src/app/pages/auth/cadastro/cadastro.component.ts
+++ src/app/pages/auth/cadastro/cadastro.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  username = '';
+  password = '';
+  confirmPassword = '';
+  error = '';
+    constructor(
+      private authService: UserService,
+      private _location: Location,
+      private _router: Router) {}
 
+      onCadastro(){}
+      goBack(): void {
+        if (window.history.length > 1) {
+          this._location.back();
+        } else {
+          this._router.navigate(['/']); 
+        }
+      }
 }
