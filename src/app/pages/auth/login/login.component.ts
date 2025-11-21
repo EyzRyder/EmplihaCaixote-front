515de3b3d2a53e8940c0eddbbe59a55102865faf
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-20 23:44:37 -0300

    implament login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import {  UserService } from '../../../services/user.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthRequest } from '../../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
 
+  constructor(
+    private authService: UserService,
+    private _location: Location,
+    private _router: Router) {}
+
+  onLogin() {
+    if (this.username.trim() == '' || this.password.trim() == '') {
+      this.error = 'Não pode deixar nenhum campo vazio';
+      return;
+    } else {
+      this.error = '';
+    }
+    const payload: AuthRequest= {
+      username: this.username,
+      password: this.password,
+    };
+    this.authService.login(payload).subscribe({
+      next: () => {
+        console.log('Logged in');
+        this._router.navigate(['/rooms']); 
+      },
+      error: (err) => {
+        this.error = err.error?.message || 'Login failed';
+      },
+    });
+  }
+
+  goBack(): void {
+    if (window.history.length > 1) {
+      this._location.back();
+    } else {
+      this._router.navigate(['/']); 
+    }
+  }
 }
