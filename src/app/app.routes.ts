b1620915e6658f0cd3e877bf366f8af7274cfc05
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-20 23:42:33 -0300

    chore: add login and cadastro route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { RoomsComponent } from './pages/rooms/rooms.component';
 import { LobbyComponent } from './pages/lobby/lobby.component';
 import { GameComponent } from './pages/game/game.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { CadastroComponent } from './pages/auth/cadastro/cadastro.component';
 
 export const routes: Routes = [
   { path: 'rooms', component: RoomsComponent },
   { path: 'room/:id', component: LobbyComponent },
   { path: 'game', component: GameComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
 
   // rota padrão
