74fe85387fc0078a1ed0ba983a7a86bdb85ccf10
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-21 01:27:31 -0300

    feat: add guard path if not log in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,12 @@ import { GameComponent } from './pages/game/game.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { CadastroComponent } from './pages/auth/cadastro/cadastro.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'rooms', component: RoomsComponent },
-  { path: 'room/:id', component: LobbyComponent },
-  { path: 'game', component: GameComponent },
+  { path: 'rooms', component: RoomsComponent ,canActivate: [authGuard],},
+  { path: 'room/:id', component: LobbyComponent ,canActivate: [authGuard],},
+  { path: 'game', component: GameComponent, canActivate: [authGuard], },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
