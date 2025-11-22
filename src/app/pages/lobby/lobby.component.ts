d79b16e739b55c57168ce82c63f63210ec76963a
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-22 02:13:04 -0300

    style: add bg

--- src/app/pages/lobby/lobby.component.ts
+++ src/app/pages/lobby/lobby.component.ts
@@ -2,10 +2,11 @@ import { Component, computed, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GameService } from '../../services/store/game.service';
 import { CommonModule } from '@angular/common';
+import { CardLayoutComponent } from '../../components/card-layout/card-layout.component';
 
 @Component({
   selector: 'app-lobby',
-  imports: [CommonModule],
+  imports: [CommonModule,CardLayoutComponent],
   templateUrl: './lobby.component.html',
   styleUrl: './lobby.component.scss'
 })
